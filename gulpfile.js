cc3a606b2ca4a08f464a0d12c6ac7677b7721279
kibs86 <kibs086@gmail.com>
2017-01-23 11:45:31 -0500

    Add version check to gulp
    - Add new task to check node version
    - Add this task as dependency to default

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,26 @@
 const gulp = require('gulp');
+const semver = require('semver');
 
-// default task
-gulp.task('default', function(done) {
-    console.log('BUILD OK');
+// check node version
+gulp.task('version', function(done) {
+
+    console.log('Checking node version: ');
+
+    const packageJson = require('./package.json');
+    const expectedVersion = packageJson.engines.node;
+    const actualVersion = process.version;
+
+    if (semver.gt(expectedVersion,actualVersion)) {
+        console.log('Incorrect node version.  Expected ' + expectedVersion + '.  Actual: ' + actualVersion);
+        process.exit(1);
+    } else {
+        console.log('Correct node version.');
+    }
     done();
 });
+
+// default task
+gulp.task('default', gulp.series('version', function(done) {
+    console.log('BUILD OK');
+    done();
+}));
