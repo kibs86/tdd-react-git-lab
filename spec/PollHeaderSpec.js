01820692cd86c5acf94bf49a6037b81075ff32d1
kibs86 <kibs086@gmail.com>
2017-01-25 14:26:35 -0500

    update pollheaderspec to check for specific message

--- spec/PollHeaderSpec.js
+++ spec/PollHeaderSpec.js
@@ -2,7 +2,20 @@ import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import PollHeader from '../src/components/PollHeader.js';
 
-describe('Header', function() {
+describe('Poll Header', function() {
+    var component;
+    beforeEach(function() {
+        component = TestUtils.renderIntoDocument(
+            <PollHeader text = "Welcome to the Poll!" />
+        );
+    });
+
+    it('prints a message', function() {
+        var actual = TestUtils.findRenderedDOMComponentWithTag(component,'h1').textContent;
+        var expected = 'Welcome to the Poll!';
+        expect(actual).toEqual(expected);
+    });
+
     it('renders without problems', function () {
         var pollheader = TestUtils.renderIntoDocument(<PollHeader />);
         expect (pollheader).toEqual(jasmine.anything());
