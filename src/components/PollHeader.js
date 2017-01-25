31d58dddd24ba99c74aff1e9b2cee5127220fd2a
kibs86 <kibs086@gmail.com>
2017-01-25 14:29:13 -0500

    update pollheader component to get message in from prop

--- src/components/PollHeader.js
+++ src/components/PollHeader.js
@@ -3,7 +3,7 @@ import React from 'react';
 class PollHeader extends React.Component{
     render(){
         return (
-            <h1>Welcome!</h1>
+            <h1>{this.props.text}</h1>
         );
     }
 }
