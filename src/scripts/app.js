29e79359ba4e097d90d5bcf2146b8cc88f943045
kibs86 <kibs086@gmail.com>
2017-01-25 08:46:20 -0500

    add react hook to index.html and update app.js to render React

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,8 +5,16 @@
 // below is same as the line after it because there's only one function in there.  Use curly braces because it's
 // a function.
 //import {greet} as sayHello from './sayHello.js';
-import * as sayHello from './sayHello.js';
+// import * as sayHello from './sayHello.js';
+//
+// window.addEventListener('load', function() {
+//     document.getElementById('welcome-message').innerHTML = sayHello.greet('World') + ' have a nice day!';
+// });
 
-window.addEventListener('load', function() {
-    document.getElementById('welcome-message').innerHTML = sayHello.greet('World') + ' have a nice day!';
-});
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('app')
+);
