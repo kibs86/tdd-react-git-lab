d5bb9647f75b436684284aa130c29825ec60c772
kibs86 <kibs086@gmail.com>
2017-01-25 14:47:42 -0500

    update container to create a constructor and set initial state and pass those down to components as props

--- src/containers/PollContainer.js
+++ src/containers/PollContainer.js
@@ -5,16 +5,28 @@ import PollAnswer from '../components/PollAnswer';
 import PollSubmitButton from '../components/PollSubmitButton';
 
 class PollContainer extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            header: 'Welcome to the poll!',
+            question: "What's your favorite food?",
+            answer1: 'Tacos',
+            answer2: 'Pizza',
+            answer3: 'Sushi',
+            correctAnswer: 'Pizza'
+        };
+    }
+
     render(){
         return (
             <div className="container">
                 <div className="col-sm-4 col-sm-offset-4 text-center">
-                    <PollHeader />
+                    <PollHeader text={this.state.header} />
                         <form>
-                            <PollQuestion />
-                            <PollAnswer />
-                            <PollAnswer />
-                            <PollAnswer />
+                            <PollQuestion text={this.state.question}/>
+                            <PollAnswer text={this.state.answer1}/>
+                            <PollAnswer text={this.state.answer2}/>
+                            <PollAnswer text={this.state.answer3}/>
                             <br />
                             <PollSubmitButton />
                         </form>
