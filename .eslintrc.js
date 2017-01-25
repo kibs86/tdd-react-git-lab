97886b587b502e3cc898dc5b3e960bfcdf6f9dab
kibs86 <kibs086@gmail.com>
2017-01-25 09:00:39 -0500

    add rules to ESLint config file for react

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,8 +33,11 @@ module.exports = {
             "error",
             "always"
         ],
-	"no-console": [
-	    "warn"
-	]
+        "no-console": [
+	        "warn"
+	    ],
+        "react/jsx-uses-react": 1,
+        "react/jsx-uses-vars": 1,
+        "react/react-in-jsx-scope": 1
     }
 };
