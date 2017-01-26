da42802c601d5ae3e7f73362f05e1d66cb519f06
kibs86 <kibs086@gmail.com>
2017-01-26 15:18:42 -0500

    update jest PollSubmitButton test so test would pass

--- src/components/__tests__/PollSubmitButtonSpec.js
+++ src/components/__tests__/PollSubmitButtonSpec.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
+import TestWrapper from '../TestWrapper.js';
 
 describe('Poll Submit Button', function() {
     const PollSubmitButton = require('../PollSubmitButton').default;
 
-    it('renders without a problem', function () {
-        var pollsubmitbutton = TestUtils.renderIntoDocument(<PollSubmitButton />);
-        var buttonText = ReactDOM.findDOMNode(pollsubmitbutton).textContent;
+    var component;
+    beforeEach(function() {
+        component = TestUtils.renderIntoDocument(
+            <TestWrapper>
+                <PollSubmitButton text = "Go" />
+            </TestWrapper>
+        );
+    });
 
-        expect(buttonText).toEqual('Go!');
+    it('renders without a problem', function () {
+        //var pollsubmitbutton = TestUtils.renderIntoDocument(<PollSubmitButton />);
+        var buttonText = ReactDOM.findDOMNode(component).textContent;
+
+        expect(buttonText).toEqual('Go');
     });
 });
