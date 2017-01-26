445706c2216bf6a1de44ea37903b63bfa3640deb
kibs86 <kibs086@gmail.com>
2017-01-26 09:10:51 -0500

    create new RadioButtonGroup component

---
+++ src/components/RadioButtonGroup.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import RadioButton from './RadioButton';
+
+class RadioButtonGroup extends React.Component{
+    render(){
+
+        const choiceItems = this.props.choices.map(choice => {
+            const {value, label} = choice;
+            const checked = value === this.props.checkedValue;
+
+            return (
+                <RadioButton
+                    key={`radio-button-${value}`}
+                    label={label}
+                    name={this.props.name}
+                    value={value}
+                    checked={checked}
+                />
+            );
+        });
+
+        return (
+            <div>
+                {choiceItems}
+            </div>
+        );
+    }
+}
+
+export default RadioButtonGroup;
